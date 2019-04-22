073fe5c4c092e7ae4202fc73a7e88f6419359c70
YongminK <yongminni97@gmail.com>
2019-04-22 17:54:50 +0300

    smoke

--- script.js
+++ script.js
@@ -11,21 +11,21 @@ var meshFloor;
 
 var crate, crateTexture, crateNormalMap, crateBumpMap;
 function init() {
-
+    clock = new THREE.Clock();
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 10000);
-
-    mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshPhongMaterial({
-            color: 0xff9999,
-            wireframe: false
-        })
-    );
-    mesh.position.y = 1;
-    mesh.receiveShadow = true;
-    mesh.castShadow = true;
-    scene.add(mesh);
+    camera.position.set(50,50,50);
+    // mesh = new THREE.Mesh(
+    //     new THREE.BoxGeometry(1, 1, 1),
+    //     new THREE.MeshPhongMaterial({
+    //         color: 0xff9999,
+    //         wireframe: false
+    //     })
+    // );
+    // mesh.position.y = 1;
+    // mesh.receiveShadow = true;
+    // mesh.castShadow = true;
+    // scene.add(mesh);
 
     meshFloor = new THREE.Mesh(
         new THREE.PlaneGeometry(20, 20, 10, 10),
@@ -42,45 +42,53 @@ function init() {
     ambientLight = new THREE.AmbientLight(0xffffff, 0.2)    ;
     scene.add(ambientLight);
 
-    light = new THREE.PointLight(0xffffff, 0.8, 18);
-    light.position.set(-3, 6 , -3);
+    light = new THREE.PointLight(0xffffff, 0.8, 10000);
+    light.position.set(-3, 1000 , -3);
     light.castShadow = true;
     light.shadow.camera.near = 0.1;
     light.shadow.camera.far = 25;
     scene.add(light);
+    geometry = new THREE.CubeGeometry( 200, 200, 200 );
+    material = new THREE.MeshLambertMaterial( { color: 0xaa6666, wireframe: false } );
+    mesh = new THREE.Mesh( geometry, material );
+    //scene.add( mesh );
+    cubeSineDriver = 0;
+ 
     
-    var textureLoader = new THREE.TextureLoader();
-    crateTexture = textureLoader.load("crate0/crate0_diffuse.png");
-    crateBumpMap = textureLoader.load("crate0/crate0_bump.png");
-    crateNormalMap = textureLoader.load("crate0/crate0_normal.png");
-    crate = new THREE.Mesh(
-        new THREE.BoxGeometry(3,3,3),
-        new THREE.MeshPhongMaterial({
-            color:0xffffff,
-            map: crateTexture,
-            bumpMap: crateBumpMap,
-            normalMap: crateNormalMap
-        })
-    );
-    scene.add(crate);
-    crate.position.set(2.5,3/2,2.5);
-    crate.recieveShadow = true;
-    crate.castShadow = true;
+    THREE.ImageUtils.crossOrigin = ''; //Need this to pull in crossdomain images from AWS
+    
+
+    light = new THREE.DirectionalLight(0xffffff,0.5);
+    light.position.set(-1,0,1);
+    scene.add(light);
+  
+    smokeTexture = THREE.ImageUtils.loadTexture('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png');
+    smokeMaterial = new THREE.MeshLambertMaterial({color: 0x00dddd, map: smokeTexture, transparent: true});
+    smokeGeo = new THREE.CubeGeometry( 50, 50, 50 );
+    smokeParticles = [];
+
+
+    for (p = 0; p < 150; p++) {
+        var particle = new THREE.Mesh(smokeGeo,smokeMaterial);
+        particle.position.set(Math.random()*500-250,Math.random()*500-250,Math.random()*1000-100);
+        particle.rotation.z = Math.random() * 360;
+        scene.add(particle);
+        smokeParticles.push(particle);
+    }
+
     var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.load('models/boat_small.mtl', function(materials){
+    mtlLoader.load('models/ship_wreck.mtl', function(materials){
         materials.preload();
         var objLoader = new THREE.OBJLoader();
         objLoader.setMaterials(materials);
-        objLoader.load('models/boat_small.obj', function(mesh){
-            mesh.scale.set(2,2,2);
-            mesh.position.set(2,2,2);ss
-            scene.add(mesh);
-           
+        objLoader.load('models/ship_wreck.obj', function(mesh){
+            mesh.scale.set(0.5,0.5,0.5);
+            scene.add(mesh);        
 
         });
     }); 
 
-    camera.position.set(0, player.height, -5);
+    
     camera.lookAt(new THREE.Vector3(0, player.height, 0));
 
     renderer = new THREE.WebGLRenderer();
@@ -91,35 +99,13 @@ function init() {
     // renderer.setClearColor(0xffffff);
 
     animate();
-
-
-
-
-
- 
-
-
-
-    // var manager = new THREE.LoadingManager();
-    // var loader = new THREE.ImageLoader(manager);
-
-
-    // var objLoader = new THREE.OBJLoader();
-    // var callbackOnLoad = function ( obj ) {
-    //     scene.add( obj );      
-    //     // camera.lookAt(obj.position)  ;
-    // };
-
-    // // load a resource from provided URL synchronously
-    // //objLoader.setLogging( true, true );
-    // objLoader.load( 'Bus-Ruin_obj.obj', callbackOnLoad);
 }
 
 function animate() {
     requestAnimationFrame(animate);
+    delta = clock.getDelta();
+    requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
     if (keyboard[87]) {
         camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
         camera.position.z -= Math.cos(camera.rotation.y) * player.speed;
@@ -145,7 +131,12 @@ function animate() {
 
     renderer.render(scene, camera);
 }
-
+function evolveSmoke() {
+    var sp = smokeParticles.length;
+    while(sp--) {
+        smokeParticles[sp].rotation.z += (delta * 0.2);
+    }
+}
 function keyDown(event) {
     keyboard[event.keyCode] = true;
 }
